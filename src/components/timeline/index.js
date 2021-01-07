a4f54cf69db3af3610091a916b2751b0b076121d
AloneH <admin@oibit.cn>
2021-01-07 16:05:30 +0800

    fix: syntax

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -2,11 +2,11 @@ import timelineComponent from './timeline.vue'
 import timelineItemComponent from './timeline-item.vue'
 
 timelineComponent.install = app => {
-  app.components(timelineComponent.name, timelineComponent)
+  app.component(timelineComponent.name, timelineComponent)
 }
 
 timelineItemComponent.install = app => {
-  app.components(timelineItemComponent.name, timelineItemComponent)
+  app.component(timelineItemComponent.name, timelineItemComponent)
 }
 
 export const timeline = timelineComponent
